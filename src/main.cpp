d4eb95e5b47d2aa448767b19fadc36c7a6f30a8a
Andres_Github <81278332+Slushi-Github@users.noreply.github.com>
2024-11-30 19:05:17 -0400

    Changed the way in which the gamepad image should be obtained.
    Even if it is changed I do not know if it is better or not, the plugin still crashes as soon as it starts, the error is when the thread starts, something is critically wrong there.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <arpa/inet.h>
 #include <coreinit/memory.h>
-#include <coreinit/thread.h>
 #include <gx2/texture.h>
 #include <jansson.h>
 #include <malloc.h>
@@ -10,10 +9,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <thread>
 #include <wups.h>
-
+#include <gx2/mem.h>
 #include "utils/logger.h"
-#include <whb/proc.h>
+#include <future>
 
 WUPS_PLUGIN_NAME("Gamepad Streamer");
 WUPS_PLUGIN_DESCRIPTION("This plugin allows you to stream the GamePad screen to a PC.");
@@ -21,194 +21,158 @@ WUPS_PLUGIN_VERSION("v0.0.1 ALPHA");
 WUPS_PLUGIN_AUTHOR("Slushi");
 WUPS_PLUGIN_LICENSE("GPL");
 
-WUPS_USE_WUT_DEVOPTAB();
-
-#define DEFAULT_IP      "192.168.10.1"
-#define DEFAULT_PORT    4242
-#define DEFAULT_FPS     30
+#define DEFAULT_IP "192.168.10.1"
+#define DEFAULT_PORT 4242
+#define DEFAULT_FPS 30
 #define DEFAULT_QUALITY 854
 
-char pcIP[16]    = DEFAULT_IP;
-uint16_t pcPort  = DEFAULT_PORT;
-uint32_t fps     = DEFAULT_FPS;
+char pcIP[16] = DEFAULT_IP;
+uint16_t pcPort = DEFAULT_PORT;
+uint32_t fps = DEFAULT_FPS;
 uint32_t quality = DEFAULT_QUALITY;
 
-GX2Texture *drcTex = nullptr;
-bool pluginActive  = true;
-OSThread threadHandle;
-
+bool pluginActive = true;
+std::thread transmissionThread;
+std::promise<bool> threadInitializationPromise;
 NotificationModuleHandle notificationHandle = 0;
 
-int initializeSocket(const char *ip, uint16_t port, sockaddr_in &addr) {
-    int socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (socket_fd < 0) {
-        std::string errorMessage = "Could not create socket.";
-        NotificationModule_UpdateDynamicNotificationText(notificationHandle, errorMessage.c_str());
-        NotificationModule_UpdateDynamicNotificationBackgroundColor(notificationHandle, {237, 28, 36, 255});
-        NotificationModule_FinishDynamicNotificationWithShake(notificationHandle, 2.0f, 0.5f);
+static bool copyBufferAndSend(GX2ColorBuffer *sourceBuffer, sockaddr_in &pcAddr, int socket_fd) {
+    GX2ColorBuffer targetBuffer = {};
+    targetBuffer.surface.width = quality;
+    targetBuffer.surface.height = (quality * 480) / 854;
+    targetBuffer.surface.format = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
+
+    GX2CalcSurfaceSizeAndAlignment(&targetBuffer.surface);
+    GX2InitColorBufferRegs(&targetBuffer);
+
+    targetBuffer.surface.image = MEMAllocFromMappedMemoryForGX2Ex(
+        targetBuffer.surface.imageSize, targetBuffer.surface.alignment);
+
+    if (!targetBuffer.surface.image) {
+        return false;
     }
 
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port   = htons(port);
-    inet_aton(ip, &addr.sin_addr);
+    GX2Invalidate(GX2_INVALIDATE_MODE_CPU, targetBuffer.surface.image, targetBuffer.surface.imageSize);
+    GX2CopySurface(&sourceBuffer->surface, 0, 0, &targetBuffer.surface, 0, 0);
 
-    return socket_fd;
+    sendto(socket_fd, targetBuffer.surface.image, targetBuffer.surface.imageSize, 0,
+           (struct sockaddr *)&pcAddr, sizeof(pcAddr));
+
+    MEMFreeToMappedMemory(targetBuffer.surface.image);
+    return true;
 }
 
-void sendGamePadImageToPC(GX2Texture *drcTex, int socket_fd, sockaddr_in &addr) {
-    if (!drcTex || !drcTex->surface.image) {
-        return;
-    }
-
-    void *data = MEMAllocFromMappedMemory(drcTex->surface.imageSize);
-    if (!data) {
-        std::string errorMessage = "Can't allocate memory for GamePad image.";
-        NotificationModule_UpdateDynamicNotificationText(notificationHandle, errorMessage.c_str());
-        NotificationModule_UpdateDynamicNotificationBackgroundColor(notificationHandle, {237, 28, 36, 255});
-        NotificationModule_FinishDynamicNotificationWithShake(notificationHandle, 2.0f, 0.5f);
-    }
-
-    memcpy(data, drcTex->surface.image, drcTex->surface.imageSize);
-    sendto(socket_fd, data, drcTex->surface.imageSize, 0, (struct sockaddr *) &addr, sizeof(addr));
-
-    MEMFreeToMappedMemory(data);
+GX2ColorBuffer *getGamePadBuffer() {
+    static GX2ColorBuffer drcBuffer = {};
+    drcBuffer.surface.width = 854;
+    drcBuffer.surface.height = 480;
+    drcBuffer.surface.format = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
+    return &drcBuffer;
 }
 
-int transmissionThread(int argc, const char **argv) {
-    sockaddr_in pcAddr;
-    int socket_fd = initializeSocket(pcIP, pcPort, pcAddr);
+void transmissionFunction() {
+    try {
+        sockaddr_in pcAddr;
+        int socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-    while (pluginActive) {
-        if (drcTex && drcTex->surface.image) {
-            sendGamePadImageToPC(drcTex, socket_fd, pcAddr);
-        }
-        OSSleepTicks(OSMillisecondsToTicks(1000 / fps));
-    }
-
-    close(socket_fd);
-    return 0;
-}
-
-void setupGamePadTexture() {
-    if (!drcTex) {
-        drcTex = (GX2Texture *) MEMAllocFromMappedMemoryForGX2Ex(sizeof(GX2Texture), 0x40);
-        if (!drcTex) {
-            std::string errorMessage = "Error while allocating GamePad texture.";
-            NotificationModule_UpdateDynamicNotificationText(notificationHandle, errorMessage.c_str());
-            NotificationModule_UpdateDynamicNotificationBackgroundColor(notificationHandle, {237, 28, 36, 255});
-            NotificationModule_FinishDynamicNotificationWithShake(notificationHandle, 2.0f, 0.5f);
+        if (socket_fd < 0) {
+            threadInitializationPromise.set_value(false);
+            return;
         }
 
-        drcTex->surface.format    = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
-        drcTex->surface.width     = quality;
-        drcTex->surface.height    = quality * 480 / 854;
-        drcTex->surface.depth     = 1;
-        drcTex->surface.tileMode  = GX2_TILE_MODE_LINEAR_ALIGNED;
-        drcTex->surface.imageSize = drcTex->surface.width * drcTex->surface.height * 4;
+        memset(&pcAddr, 0, sizeof(pcAddr));
+        pcAddr.sin_family = AF_INET;
+        pcAddr.sin_port = htons(pcPort);
+        inet_aton(pcIP, &pcAddr.sin_addr);
 
-        GX2InitTextureRegs(drcTex);
-        drcTex->surface.use = (GX2SurfaceUse) (GX2_SURFACE_USE_COLOR_BUFFER | GX2_SURFACE_USE_TEXTURE);
+        threadInitializationPromise.set_value(true);
 
-        if (drcTex->surface.imageSize) {
-            drcTex->surface.image = MEMAllocFromMappedMemoryForGX2Ex(
-                    drcTex->surface.imageSize,
-                    drcTex->surface.alignment);
-
-            if (!drcTex->surface.image) {
-                std::string errorMessage = "Error while allocating GamePad image";
-                NotificationModule_UpdateDynamicNotificationText(notificationHandle, errorMessage.c_str());
-                NotificationModule_UpdateDynamicNotificationBackgroundColor(notificationHandle, {237, 28, 36, 255});
-                NotificationModule_FinishDynamicNotificationWithShake(notificationHandle, 2.0f, 0.5f);
+        while (pluginActive) {
+            GX2ColorBuffer *gamePadBuffer = getGamePadBuffer();
+            if (gamePadBuffer) {
+                copyBufferAndSend(gamePadBuffer, pcAddr, socket_fd);
             }
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000 / fps));
         }
+
+        close(socket_fd);
+    } catch (const std::exception &e) {
+        threadInitializationPromise.set_value(false);
     }
 }
 
 void loadConfigFromJson() {
-    char configFilePath[256] = "fs:/vol/external01/wiiu/environments/aroma/plugins/gamepadStreamerConfig.json";
-
+    char configFilePath[256] = "fs:/vol/external01/wiiu/gamepadStreamerConfig.json";
     FILE *file = fopen(configFilePath, "r");
     if (!file) {
-        OSFatal("[gamepadStreamer - loadConfigFromJson] Failed to open configuration file");
         return;
     }
 
     fseek(file, 0, SEEK_END);
     long fileSize = ftell(file);
     fseek(file, 0, SEEK_SET);
-    char *jsonData = (char *) malloc(fileSize + 1);
+    char *jsonData = (char *)malloc(fileSize + 1);
+    if (!jsonData) {
+        fclose(file);
+        return;
+    }
+
     fread(jsonData, 1, fileSize, file);
     jsonData[fileSize] = '\0';
     fclose(file);
 
     json_error_t error;
     json_t *root = json_loads(jsonData, 0, &error);
+    free(jsonData);
+
     if (!root) {
-        OSFatal("[gamepadStreamer - loadConfigFromJson] Error parsing JSON: ");
-        free(jsonData);
         return;
     }
 
-    json_t *ipJson      = json_object_get(root, "pc_ip");
-    json_t *fpsJson     = json_object_get(root, "fps");
+    json_t *ipJson = json_object_get(root, "pc_ip");
+    json_t *fpsJson = json_object_get(root, "fps");
     json_t *qualityJson = json_object_get(root, "image_quality");
 
     if (json_is_string(ipJson)) {
         strncpy(pcIP, json_string_value(ipJson), sizeof(pcIP) - 1);
         pcIP[sizeof(pcIP) - 1] = '\0';
     }
+
     if (json_is_integer(fpsJson)) {
-        fps = (uint32_t) json_integer_value(fpsJson);
+        fps = (uint32_t)json_integer_value(fpsJson);
     }
+
     if (json_is_integer(qualityJson)) {
-        quality = (uint32_t) json_integer_value(qualityJson);
+        quality = (uint32_t)json_integer_value(qualityJson);
     }
 
     json_decref(root);
-    free(jsonData);
 }
 
 INITIALIZE_PLUGIN() {
-    initLogging();
-    setupGamePadTexture();
     loadConfigFromJson();
     pluginActive = true;
-
-    char stack[0x2000];
-    if (OSCreateThread(&threadHandle, transmissionThread, 0, nullptr, stack, sizeof(stack), 16, OS_THREAD_ATTRIB_AFFINITY_ANY)) {
-        OSResumeThread(&threadHandle);
-    } else {
-        OSFatal("[gamepadStreamer - INITIALIZE_PLUGIN] Failed to create transmission thread");
+    std::future<bool> threadStarted = threadInitializationPromise.get_future();
+    transmissionThread = std::thread(transmissionFunction);
+    if (!threadStarted.get()) {
+        pluginActive = false;
+        if (transmissionThread.joinable()) {
+            transmissionThread.join();
+        }
+        return;
     }
-    deinitLogging();
-
-    std::string errorMessage = "GamePadStreamer initialized";
-    NotificationModule_UpdateDynamicNotificationText(notificationHandle, errorMessage.c_str());
-    NotificationModule_UpdateDynamicNotificationBackgroundColor(notificationHandle, {143, 217, 209, 255});
-    NotificationModule_FinishDynamicNotificationWithShake(notificationHandle, 2.0f, 0.5f);
 }
 
 DEINITIALIZE_PLUGIN() {
     pluginActive = false;
-    OSJoinThread(&threadHandle, nullptr);
-    if (drcTex && drcTex->surface.image) {
-        MEMFreeToMappedMemory(drcTex->surface.image);
+    if (transmissionThread.joinable()) {
+        transmissionThread.join();
     }
-    if (drcTex) {
-        MEMFreeToMappedMemory(drcTex);
-    }
-    DEBUG_FUNCTION_LINE("DEINITIALIZE_PLUGIN of gamepadStreamer!");
 }
 
 ON_APPLICATION_REQUESTS_EXIT() {
     pluginActive = false;
-    OSJoinThread(&threadHandle, nullptr);
-    if (drcTex && drcTex->surface.image) {
-        MEMFreeToMappedMemory(drcTex->surface.image);
+    if (transmissionThread.joinable()) {
+        transmissionThread.join();
     }
-    if (drcTex) {
-        MEMFreeToMappedMemory(drcTex);
-    }
-    DEBUG_FUNCTION_LINE("ON_APPLICATION_REQUESTS_EXIT of gamepadStreamer!");
 }
